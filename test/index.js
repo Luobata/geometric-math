abb4c6fbbed07529cbf87f07b79e056993bcdefc
yaochenghao <yaochenghao@bytedance.com>
2018-06-30 17:56:53 +0800

    add: test case

--- test/index.js
+++ test/index.js
@@ -11,14 +11,17 @@ window.onload = () => {
 
     canvas.style.width = `${width}px`;
     canvas.style.height = `${height}px`;
+    canvas.style.margin = '200px 0 0 200px';
 
     document.body.appendChild(canvas);
 
     const g1 = [{ x: 10, y: 10 }, { x: 200, y: 10 }, { x: 150, y: 150 }];
+    const g2 = [{ x: 300, y: 100 }, { x: 200, y: 50 }, { x: 100, y: 150 }];
 
-    const renderPoint = p => {
+    const renderPoint = (p, color = 'black') => {
         ctx.save();
         ctx.beginPath();
+        ctx.strokeStyle = color;
         ctx.moveTo(p[0].x * pixelRatio, p[0].y * pixelRatio);
         for (let i = 1; i < p.length; i++) {
             const item = p[i];
@@ -31,6 +34,7 @@ window.onload = () => {
 
     const render = () => {
         renderPoint(g1);
+        renderPoint(g2, 'red');
     };
 
     render();
