ae0238e3a8333c5363da86b4432e8355e79c21dd
yaochenghao <yaochenghao@bytedance.com>
2018-07-05 09:26:09 +0800

    add: Melkman debug

--- src/algorithm/convex-hull/Melkman.ts
+++ src/algorithm/convex-hull/Melkman.ts
@@ -11,14 +11,15 @@ export default (l: IPointList): IPointList => {
     const stackH: IPointList = [l[0], l[1]];
     const stackT: IPointList = [l[0], l[l.length - 1]];
 
-    const judge: Function = (a: IPoint): boolean => {
+    // 判断点在多边形内部
+    const inner: Function = (a: IPoint): boolean => {
         const lastH: Vector = new Vector({
             x: stackH[stackH.length - 1].x - stackH[stackH.length - 2].x,
             y: stackH[stackH.length - 1].y - stackH[stackH.length - 2].y,
         });
         const nowH: Vector = new Vector({
-            x: a.x - stackH[stackH.length - 1].x,
-            y: a.y - stackH[stackH.length - 1].y,
+            x: a.x - stackH[stackH.length - 2].x,
+            y: a.y - stackH[stackH.length - 2].y,
         });
 
         const lastT: Vector = new Vector({
@@ -26,18 +27,44 @@ export default (l: IPointList): IPointList => {
             y: stackT[stackT.length - 1].y - stackT[stackT.length - 2].y,
         });
         const nowT: Vector = new Vector({
-            x: a.x - stackT[stackT.length - 1].x,
-            y: a.y - stackT[stackT.length - 1].y,
+            x: a.x - stackT[stackT.length - 2].x,
+            y: a.y - stackT[stackT.length - 2].y,
         });
 
-        return nowH.cross(lastH) * nowT.cross(lastH) >= 0;
+        return nowH.cross(lastH) > 0 && nowT.cross(lastT) < 0;
+    };
+
+    const judge: Function = (
+        a: IPoint,
+        stack: IPointList,
+        dir: number = 1,
+    ): boolean => {
+        const last: Vector = new Vector({
+            x: stack[stack.length - 1].x - stack[stack.length - 2].x,
+            y: stack[stack.length - 1].y - stack[stack.length - 2].y,
+        });
+        const now: Vector = new Vector({
+            x: a.x - stack[stack.length - 1].x,
+            y: a.y - stack[stack.length - 1].y,
+        });
+
+        return last.cross(now) * dir >= 0;
     };
     // tslint:disable-next-line prefer-for-of
     for (let i: number = 2; i < l.length - 1; i = i + 1) {
-        if (judge(l[i])) {
-            // stackH.push(l[i]);
+        if (!inner(l[i])) {
+            while (stackH.length > 2 && !judge(l[i], stackH)) {
+                stackH.pop();
+            }
+            while (stackT.length > 2 && !judge(l[i], stackT, -1)) {
+                stackT.pop();
+            }
+            stackH.push(l[i]);
+            stackT.push(l[i]);
         }
     }
+    console.log(stackH);
+    console.log(stackT);
 
     return [...stackH, l[l.length - 1]];
 };
